eab6830e6aad816d416cd2a9627845ca10106e9b
lipengzhou <lpzmail@163.com>
2018-05-15 17:29:46 +0800

    添加Home组件并配置路由表&登陆完成跳转到Home组件

--- src/components/login/script.js
+++ src/components/login/script.js
@@ -14,12 +14,10 @@ export default {
     handleLogin () {
       axios.post('http://localhost:8888/api/private/v1/login', this.loginForm)
         .then(res => {
-          // const data = res.data.data
-          // const meta = res.data.meta
           const {data, meta} = res.data
           const {msg, status} = meta
           if (status === 200) {
-            window.alert('恭喜，登陆成功！')
+            this.$router.push('/')
           } else if (status === 400) {
             window.alert(msg)
           }
--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,16 @@ import Router from 'vue-router'
 // @ 是 src 目录的绝对路径
 // @ 后面的 / 别忘了
 import Login from '@/components/login/login'
+import Home from '@/components/home/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      component: Home
+    },
     {
       path: '/login',
       component: Login
