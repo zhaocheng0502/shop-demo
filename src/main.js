ccd842de85272f731d7cb0d3c607476cf7f35270
lipengzhou <lpzmail@163.com>
2018-05-15 16:12:11 +0800

    :tada: Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 该规则用以 eslint 代码规范校验的临时规避
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
