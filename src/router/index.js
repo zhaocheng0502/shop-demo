707238296074ac02c22c8620d760b440a1550332
lipengzhou <lpzmail@163.com>
2018-05-15 16:29:47 +0800

    添加登陆组件并配置路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// 加载路径中可以使用 @ 绝对路径
+// @ 是 src 目录的绝对路径
+// @ 后面的 / 别忘了
+import Login from '@/components/login/login'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      component: Login
+    }
   ]
 })
