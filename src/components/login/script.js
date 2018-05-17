e131dd1e447b8566261026396fb2fa8b75ac4dcb
lipengzhou <lpzmail@163.com>
2018-05-17 12:54:41 +0800

    使用路由守卫拦截非登陆视图导航

--- src/components/login/script.js
+++ src/components/login/script.js
@@ -17,6 +17,10 @@ export default {
           const {data, meta} = res.data
           const {msg, status} = meta
           if (status === 200) {
+            // 将凭证放到到本地存储（会在路由守卫那里使用）
+            window.localStorage.setItem('token', data.token)
+
+            // 跳转到首页
             this.$router.push('/')
           } else if (status === 400) {
             window.alert(msg)
--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import Home from '@/components/home/home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -21,3 +21,20 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const {path} = to
+  if (path !== '/login') { // 如果请求的不是 /login 则校验登陆状态
+    const token = window.localStorage.getItem('token')
+    if (!token) { // 如果没有 token 则让其跳转到 /login
+      next('/login')
+    } else { // 有 token，让其通过
+      next()
+    }
+  } else {
+    // 如果用户请求的就是 /login 则直接调用 next() 放行
+    next()
+  }
+})
+
+export default router
