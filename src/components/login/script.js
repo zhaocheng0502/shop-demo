3286fabe3e88da028511e9fbcbc6839b1f3850e9
lipengzhou <lpzmail@163.com>
2018-05-17 13:47:28 +0800

    为登陆增加表单验证

--- src/components/login/script.js
+++ src/components/login/script.js
@@ -6,26 +6,40 @@ export default {
       loginForm: {
         username: '',
         password: ''
+      },
+      loginFormRule: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
       }
     }
   },
 
   methods: {
     handleLogin () {
-      axios.post('http://localhost:8888/api/private/v1/login', this.loginForm)
-        .then(res => {
-          const {data, meta} = res.data
-          const {msg, status} = meta
-          if (status === 200) {
-            // 将凭证放到到本地存储（会在路由守卫那里使用）
-            window.localStorage.setItem('token', data.token)
+      // ['form'] 中的 form 就是 el-form 标签 ref 属性值
+      this.$refs['form'].validate((valid) => {
+        if (!valid) {
+          return
+        }
+        axios.post('http://localhost:8888/api/private/v1/login', this.loginForm)
+          .then(res => {
+            const {data, meta} = res.data
+            const {msg, status} = meta
+            if (status === 200) {
+              // 将凭证放到到本地存储（会在路由守卫那里使用）
+              window.localStorage.setItem('token', data.token)
 
-            // 跳转到首页
-            this.$router.push('/')
-          } else if (status === 400) {
-            window.alert(msg)
-          }
-        })
+              // 跳转到首页
+              this.$router.push('/')
+            } else if (status === 400) {
+              window.alert(msg)
+            }
+          })
+      })
     }
   }
 }
