8ac38496487aa9cca48d53c85a8609d881faf2e0
lipengzhou <lpzmail@163.com>
2018-05-17 16:03:10 +0800

    用户退出-加入友好交互提示

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -80,11 +80,27 @@ export default {
       console.log(key, keyPath)
     },
     handleLogout () {
-      // 1. 删除本地存储中的 token，也就是清除登陆状态
-      window.localStorage.removeItem('token')
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => { // 用户点击 确定 执行这里
+        // 1. 删除本地存储中的 token，也就是清除登陆状态
+        window.localStorage.removeItem('token')
 
-      // 2. 跳转到 /login
-      this.$router.push('/login')
+        // 2. 跳转到 /login
+        this.$router.push('/login')
+
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => { // 用户点击 取消 执行这里
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
     }
   }
 }
