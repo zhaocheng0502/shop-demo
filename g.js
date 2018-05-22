bce8f143c76e27d2bc7b17ccae89889111a13caa
lipengzhou <lpzmail@163.com>
2018-05-22 11:56:00 +0800

    使用脚本自动创建组件结构

---
+++ g.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+
+const componentName = process.argv[2]
+
+const vueTpl = `<template src="./template.html"></template>
+<script src="./script.js"></script>
+<style src="./style.css"></style>
+`
+
+const templateTpl = `<div class="${componentName}-wrap">
+  <p>${componentName} component</p>
+</div>
+`
+
+const scriptTpl = `export default {
+  created () {},
+  data () {
+    return {}
+  },
+  methods: {}
+}
+`
+
+const styleTpl = `.${componentName}-wrap {
+  /* style here */
+}
+`
+
+fs.mkdir(`./src/components/${componentName}`, err => {
+  if (err) {
+    console.error(`组件创建失败！`)
+    console.error('    ' + err.message)
+    return
+  }
+  fs.writeFile(`./src/components/${componentName}/index.vue`, vueTpl,() => {})
+  fs.writeFile(`./src/components/${componentName}/template.html`, templateTpl, () => {})
+  fs.writeFile(`./src/components/${componentName}/scirpt.js`, scriptTpl, () => {})
+  fs.writeFile(`./src/components/${componentName}/style.css`, styleTpl, () => {})
+  console.log('组件创建成功！')
+})
