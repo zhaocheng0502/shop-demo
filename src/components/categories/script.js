5d18c83251846e4b56e79885769acc79ed5203a3
lipengzhou <lpzmail@163.com>
2018-05-22 16:02:54 +0800

    商品分类-分页+loading

--- src/components/categories/script.js
+++ src/components/categories/script.js
@@ -5,18 +5,34 @@ export default {
   data () {
     return {
       tableData: [],
+      total: 0,
+      loading: true
     }
   },
   methods: {
-    async loadCategories () {
+    async loadCategories (page = 1) {
+      this.loading = true
       const res = await this.$http({
         url: '/categories',
-        method: 'get'
+        method: 'get',
+        params: {
+          pagenum: page,
+          pagesize: 5
+        }
       })
       const { meta, data } = res.data
       if (meta.status === 200) {
-        this.tableData = data
+        this.total = data.total
+        this.tableData = data.result
+        this.loading = false // 取消 loading 效果
       }
+    },
+
+    /**
+     * 处理分页页码改变
+     */
+    handleCurrentChange (page) {
+      this.loadCategories(page)
     }
   }
 }
