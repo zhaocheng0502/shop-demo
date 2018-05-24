97245e6eaf0e86e14cc2c3329201148bdd5babdf
lipengzhou <lpzmail@163.com>
2018-05-24 10:59:51 +0800

    添加商品-基本信息

--- src/components/goods-add/script.js
+++ src/components/goods-add/script.js
@@ -1,10 +1,46 @@
 export default {
   created () {
+    this.loadCategories()
   },
   data () {
     return {
-      form: {}
+      form: {
+        goods_name: '',
+        goods_cat: [],
+        goods_price: '',
+        goods_number: '',
+        goods_weight: '',
+        is_promote: false
+      },
+      options: []
     }
   },
-  methods: {}
+  methods: {
+    async loadCategories () {
+      const res = await this.$http({
+        url: '/categories',
+        method: 'get',
+        params: {
+          type: 3 // 只加载二级分类列表数据
+        }
+      })
+      const {meta, data} = res.data
+      if (meta.status === 200) {
+        this.options = data
+      }
+    },
+
+    async handleAdd () {
+      const res = await this.$http({
+        url: '/goods',
+        method: 'post',
+        data: {
+          ...this.form,
+          goods_cat: this.form.goods_cat.join(',')
+        }
+      })
+
+      console.log(res.data)
+    }
+  }
 }
