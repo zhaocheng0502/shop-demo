30d42d213865f742708a57d61c98a4160634f3ae
lipengzhou <lpzmail@163.com>
2018-05-24 14:54:26 +0800

    添加商品-完成图片上传处理

--- src/components/goods-add/script.js
+++ src/components/goods-add/script.js
@@ -10,18 +10,33 @@ export default {
         goods_price: '',
         goods_number: '',
         goods_weight: '',
-        is_promote: false
+        is_promote: false,
+        pics: [
+          // {
+          //   "pic": "tmp_uploads/2fd6791f5aa96f5bf3e36a8af1e177dd.png"
+          // }
+        ]
       },
-      options: []
+      options: [],
+      fileList: [
+        // {name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'},
+        // {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}
+      ],
+      headers: {
+        Authorization: window.localStorage.getItem('token')
+      }
     }
   },
   methods: {
+    /**
+     * 加载商品分类
+     */
     async loadCategories () {
       const res = await this.$http({
         url: '/categories',
         method: 'get',
         params: {
-          type: 3 // 只加载二级分类列表数据
+          type: 3
         }
       })
       const {meta, data} = res.data
@@ -30,6 +45,9 @@ export default {
       }
     },
 
+    /**
+     * 添加商品
+     */
     async handleAdd () {
       const res = await this.$http({
         url: '/goods',
@@ -41,6 +59,20 @@ export default {
       })
 
       console.log(res.data)
+    },
+
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+
+    handlePreview(file) {
+      console.log(file)
+    },
+
+    handleUploadSuccess (response, file, fileList) {
+      this.form.pics.push({
+        pic: response.data.tmp_path
+      })
     }
   }
 }
