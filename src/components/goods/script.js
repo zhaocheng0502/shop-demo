48afb4c5fa268b506cac60c8205b528fd3e0a81c
lipengzhou <lpzmail@163.com>
2018-05-24 09:35:49 +0800

    商品列表-简单分页

--- src/components/goods/script.js
+++ src/components/goods/script.js
@@ -4,23 +4,32 @@ export default {
   },
   data () {
     return {
-      tableData: []
+      tableData: [],
+      total: 0,
+      loading: true
     }
   },
   methods: {
-    async loadGoods () {
+    async loadGoods (page = 1) {
+      this.loading = true
       const res = await this.$http({
         url: '/goods',
         method: 'get',
         params: {
-          pagenum: 1,
+          pagenum: page,
           pagesize: 5
         }
       })
       const {meta, data} = res.data
       if (meta.status === 200) {
         this.tableData = data.goods
+        this.total = data.total
+        this.loading = false
       }
+    },
+
+    handleCurrentChange (page) {
+      this.loadGoods(page)
     }
   }
 }
